e31b337c8693e985666afd80eb3ae1d39a93e9de
Greg Rotter <grotter@gmail.com>
2018-12-13 13:19:07 -0800

    scaffolding for TM1637 display

--- ArduinoAQI.ino
+++ ArduinoAQI.ino
@@ -1,7 +1,11 @@
 #include <Button.h>
+#include <SevenSegmentTM1637.h>
 #include "ArduinoAQIData.h"
 
-Button resetButton(12);
+#define DISPLAY_LENGTH 4
+
+Button resetButton(PIN_RESET);
+SevenSegmentTM1637 display(PIN_LED_CLK, PIN_LED_DIO);
 ArduinoAQIData data;
 
 void setup() {
@@ -14,12 +18,31 @@ void setup() {
   // LED_BUILTIN on the WeMos D1 R2 is inverted
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  
+
+  // initialize display
+  display.begin();
+  display.setBacklight(25);
+
+  // connect to WiFi
   data.begin();
+
+  // initialize WiFi reset button
   resetButton.begin();
 }
 
-void loop() {
+char* getNumberWithLeadingZeros(long num) {
+  return getNumberWithLeadingZeros(num, DISPLAY_LENGTH);
+}
+
+char* getNumberWithLeadingZeros(long num, int displayLength) {
+  char buffer[displayLength];
+  String pattern = "%0" + String(displayLength) + "d";
+  sprintf(buffer, pattern.c_str(), num);
+  
+  return buffer;
+}
+
+void loop() {    
   if (data.isConnected()) {
     digitalWrite(LED_BUILTIN, LOW);
   } else {
@@ -35,4 +58,9 @@ void loop() {
       Serial.println("ThingSpeak write error!");
     }
   }
+
+  long num = random(0, 9999);
+  display.clear();
+  display.print(getNumberWithLeadingZeros(num));
+  delay(300);
 }
--- config.h
+++ config.h
@@ -1,3 +1,6 @@
 #define ACCESS_POINT "Arduino AQI Setup"
 #define THINGSPEAK_REGISTRY_CHANNEL_NUMBER 0
 #define THINGSPEAK_REGISTRY_API_KEY ""
+#define PIN_LED_CLK 14 // SCK D5 on WeMos D1 R2
+#define PIN_LED_DIO 12 // MISO D6 on WeMos D1 R2
+#define PIN_RESET 13 // MOSI D7 on WeMos D1 R2
