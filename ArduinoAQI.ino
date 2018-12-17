ae10eeb7eb9d71f8fc75b8f52739b6d332d6e16e
Greg Rotter <grotter@gmail.com>
2018-12-17 13:50:58 -0800

    first pass at sensor integration

--- ArduinoAQI.ino
+++ ArduinoAQI.ino
@@ -1,15 +1,21 @@
+#include <SoftwareSerial.h>
 #include <Button.h>
 #include <SevenSegmentTM1637.h>
+#include <PMS.h>
 #include "ArduinoAQIData.h"
-
-#define DISPLAY_LENGTH 4
 
 Button resetButton(PIN_RESET);
 SevenSegmentTM1637 display(PIN_LED_CLK, PIN_LED_DIO);
+
+SoftwareSerial pmsSerial(PIN_PMS_TX, PIN_PMS_RX);
+PMS pms(pmsSerial);
+PMS::DATA pmsData;
+
 ArduinoAQIData data;
 
 void setup() {
   Serial.begin(9600);
+  pmsSerial.begin(9600);
 
   Serial.println("");
   Serial.println("Arduino AQI v1.0");
@@ -23,9 +29,9 @@ void setup() {
   display.begin();
   display.setBacklight(25);
 
-  // connect to WiFi
-  // note: access point mode is blocking
-  data.begin();
+//  // connect to WiFi
+//  // note: access point mode is blocking
+//  data.begin();
 
   // initialize WiFi reset button
   resetButton.begin();
@@ -43,7 +49,20 @@ char* getNumberWithLeadingZeros(long num, int displayLength) {
   return buffer;
 }
 
-void loop() {    
+void loop() {  
+  if (pms.read(pmsData)) {
+    Serial.print("PM 1.0 (ug/m3): ");
+    Serial.println(pmsData.PM_AE_UG_1_0);
+
+    Serial.print("PM 2.5 (ug/m3): ");
+    Serial.println(pmsData.PM_AE_UG_2_5);
+
+    Serial.print("PM 10.0 (ug/m3): ");
+    Serial.println(pmsData.PM_AE_UG_10_0);
+
+    Serial.println();
+  }
+      
   if (data.isConnected()) {
     digitalWrite(LED_BUILTIN, LOW);
   } else {
@@ -60,8 +79,9 @@ void loop() {
     }
   }
 
-  long num = random(0, 9999);
-  display.clear();
-  display.print(getNumberWithLeadingZeros(num));
-  delay(300);
+//  // testing the display
+//  long num = random(0, 9999);
+//  display.clear();
+//  display.print(getNumberWithLeadingZeros(num));
+//  delay(300);
 }
